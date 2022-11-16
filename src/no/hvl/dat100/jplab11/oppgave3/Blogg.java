9a83f9a36ddaa54c5d22072263cf231a892398aa
henti <henti@DESKTOP-IFCOL96>
2022-11-16 22:30:40 +0100

    ferdig

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -2,71 +2,117 @@ package no.hvl.dat100.jplab11.oppgave3;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
+import no.hvl.dat100.jplab11.oppgave2.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggtabell;
+	private int nesteLedige;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		int str = 20;
+		innleggtabell = new Innlegg[str];
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[lengde];
+
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedige;
 	}
-	
-	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
 
+	public Innlegg[] getSamling() {
+		return innleggtabell;
 	}
-	
+
 	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innlegg.erLik(innleggtabell[i])) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+
+		if (finnInnlegg(innlegg) != -1) {
+			return true;
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i] == null) {
+				return true;
+			}
+		}
+		return false;
 
 	}
-	
+
 	public boolean leggTil(Innlegg innlegg) {
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innlegg.erLik(innleggtabell[i])) {
+				return false;
+			}
+		}
 
-		throw new UnsupportedOperationException(TODO.method());
+		innleggtabell[nesteLedige] = innlegg;
+		nesteLedige++;
+
+		return true;
 	}
-	
+
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String n = "\n";
+		String string = "";
+
+		for (int i = 0; i < innleggtabell.length; i++) {
+			string += innleggtabell[i].toString();
+		}
+
+		return getAntall() + n + string;
 	}
 
 	// valgfrie oppgaver nedenfor
-	
+
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] temp = innleggtabell;
+		innleggtabell = new Innlegg[innleggtabell.length * 2];
+
+		for (int i = 0; i < temp.length; i++) {
+			innleggtabell[i] = temp[i];
+		}
 	}
-	
+
 	public boolean leggTilUtvid(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 	}
-	
+
 	public boolean slett(Innlegg innlegg) {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public int[] search(String keyword) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+
+		int ant = 0;
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (keyword == innleggtabell[i].getTekst()) {
+				ant++;
+			}
+		}
+
+		int[] ider = new int[ant];
+		return ider;
 
 	}
 }
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -2,6 +2,7 @@ package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -10,6 +11,20 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		try {
+			File file = new File(mappe + filnavn);
+			PrintWriter writer = new PrintWriter(file);
+
+			writer.println(samling.getAntall()+ 10);
+			for (int i = 0; i < samling.getAntall(); i++) {
+				writer.print(samling.getSamling()[i].toString());
+			}
+			writer.close();
+			return true;
+
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+
 	}
 }
